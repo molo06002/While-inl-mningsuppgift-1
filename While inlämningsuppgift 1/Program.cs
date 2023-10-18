3797d3c0fa2c7ff0c8f04d1b970b632034d583c4
molo06002 <molo06002@E-R90YE07N>
2023-10-18 12:39:00 +0200

    Add project files.

---
+++ While inlämningsuppgift 1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace inlämnings_uppgift_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tal = 1;
+            int tal2 = 200;
+            int tal3 = 1000;
+            while (tal < 30)
+            {
+                tal++;
+                Console.WriteLine(tal);
+            }
+            while (tal2 > 180)
+            {
+                tal2--;
+                Console.WriteLine(tal2);
+            }
+            for (tal3 = 1000; tal3 <= 1400; tal3 += 50)
+            {
+                Console.WriteLine(tal3);
+            }
+            
+        }
+    }
+}
